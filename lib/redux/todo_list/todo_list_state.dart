da9a00308abd136871e2e742251b75f37f13f116
lightlitebug <light.lite.bug@gmail.com>
2022-12-01 15:40:12 +0900

    Modified TodoListState

--- lib/redux/todo_list/todo_list_state.dart
+++ lib/redux/todo_list/todo_list_state.dart
@@ -1,34 +1,49 @@
 import 'package:equatable/equatable.dart';
 
+import '../../models/custom_error.dart';
 import '../../models/todo_model.dart';
 
+enum TodoListStatus {
+  initial,
+  loading,
+  success,
+  failure,
+}
+
 class TodoListState extends Equatable {
+  final TodoListStatus status;
   final List<Todo> todos;
+  final CustomError error;
   const TodoListState({
+    required this.status,
     required this.todos,
+    required this.error,
   });
 
   factory TodoListState.initial() {
-    return TodoListState(
-      todos: <Todo>[
-        Todo(id: '1', todoDesc: 'Clean the room'),
-        Todo(id: '2', todoDesc: 'Wash the dish'),
-        Todo(id: '3', todoDesc: 'Do homework'),
-      ],
+    return const TodoListState(
+      status: TodoListStatus.initial,
+      todos: [],
+      error: CustomError(),
     );
   }
 
   @override
-  List<Object> get props => [todos];
+  List<Object> get props => [status, todos, error];
 
   @override
-  String toString() => 'TodoListState(todos: $todos)';
+  String toString() =>
+      'TodoListState(status: $status, todos: $todos, error: $error)';
 
   TodoListState copyWith({
+    TodoListStatus? status,
     List<Todo>? todos,
+    CustomError? error,
   }) {
     return TodoListState(
+      status: status ?? this.status,
       todos: todos ?? this.todos,
+      error: error ?? this.error,
     );
   }
 }
